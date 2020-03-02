b6de8ff65420ff57ebc7bb88f719e58fcdd03aab
Liying Wang <jaybin502@gmail.com>
2020-03-01 19:55:38 -0800

    initial commit

---
+++ R/hello.R
@@ -0,0 +1,19 @@
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+#' Adds two to any number
+#'
+#' @param x an integer or double; any number
+#'
+#' @return a number
+#' @export
+#'
+#' @examples
+#' plus_two(1)
+#'
+plus_two <- function(x) {
+
+  x + 2
+
+}
---
+++ R/test_3.R
@@ -0,0 +1,15 @@
+#' Adds three to any number
+#'
+#' @param x an integer or double; any number
+#'
+#' @return a number
+#' @export
+#'
+#' @examples
+#' plus_two(1)
+#'
+plus_three <- function(x) {
+
+  x + 3
+
+}
