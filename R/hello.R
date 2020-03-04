7c2e96f87ecefea32b0d50cc35895029124a0f3d
Liying Wang <jaybin502@gmail.com>
2020-03-03 23:44:31 -0800

    correction on test file

--- R/hello.R
+++ R/hello.R
@@ -15,5 +15,19 @@
 plus_two <- function(x) {
 
   x + 2
+}
+
+#' Adds three to any number
+#'
+#' @param x an integer or double; any number
+#'
+#' @return a number
+#' @export
+#'
+#' @examples
+#' plus_three(1)
+#'
+plus_three <- function(x) {
+  x + 3
 
 }
--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -1,3 +1,18 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("plus_two works", {
+
+  output2 <- plus_two(2)
+  outputminus2 <- plus_two(-2)
+
+  expect_equal(output2, 4)
+  expect_equal(outputminus2, 0)
+})
+
+
+test_that("plus_three works", {
+
+  output3 <- plus_three(2)
+  outputminus3 <- plus_three(-2)
+
+  expect_equal(output3, 5)
+  expect_equal(outputminus3, 1)
 })
